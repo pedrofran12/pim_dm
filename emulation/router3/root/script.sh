493fe38df9a131818c24585a0a1018f64168cbbf
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2017-11-14 11:06:27 +0000

    Test neighbors

--- emulation/router3/root/script.sh
+++ emulation/router3/root/script.sh
@@ -1,12 +1,6 @@
 rm -rf MulticastRouting/
 cp -rf /hosthome/Desktop/pim/ MulticastRouting/
 cd MulticastRouting
-pip-3.2 install -r requirements.txt
+pip-3.2 install --index-url=https://pypi.python.org/simple/ -r requirements.txt
 
-python3 Run.py -stop
-python3 Run.py -start
-python3 Run.py -aiigmp eth0
-python3 Run.py -aiigmp eth1
-python3 Run.py -ai eth0
-python3 Run.py -ai eth1
-python3 Run.py -v
+python3 Server.py
