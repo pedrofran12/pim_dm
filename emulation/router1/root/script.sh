d8a7048d92650805df66ba5439cca8144d8cb019
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2018-07-19 17:34:20 +0100

    test IGMP and results

--- emulation/router1/root/script.sh
+++ emulation/router1/root/script.sh
@@ -1,14 +1,10 @@
 rm -rf MulticastRouting/
-cp -rf /hosthome/Desktop/pim/ MulticastRouting/
+cp -rf /hosthome/Desktop/new_protocol2/ MulticastRouting/
 cd MulticastRouting
-pip-3.2 install --index-url=https://pypi.python.org/simple/ -r requirements.txt
+#pip-3.2 install --index-url=https://pypi.python.org/simple/ -r requirements.txt
 
 python3 Run.py -stop
 python3 Run.py -start
+python3 Run.py -t R1 10.0.0.5
 python3 Run.py -aiigmp eth0
-python3 Run.py -aiigmp eth1
-python3 Run.py -aiigmp eth2
-python3 Run.py -ai eth0
-python3 Run.py -ai eth1
-python3 Run.py -ai eth2
 python3 Run.py -v
---
+++ emulation/switch1/root/script.sh
@@ -0,0 +1,3 @@
+rm /hosthome/Desktop/test_capture.pcap
+tcpdump -i br0 -Q in -w /hosthome/Desktop/test_capture.pcap
+
