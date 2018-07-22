8c03503030137ff84c300e56349732be5ae89581
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2018-07-22 17:15:55 +0100

    Test neighbors/hello using logging events

--- emulation/router2/root/script.sh
+++ emulation/router2/root/script.sh
@@ -1,6 +1,9 @@
 rm -rf MulticastRouting/
 cp -rf /hosthome/Desktop/pim/ MulticastRouting/
 cd MulticastRouting
-pip-3.2 install --index-url=https://pypi.python.org/simple/ -r requirements.txt
 
-python3 Server.py
+python3 Run.py -stop
+python3 Run.py -start
+python3 Run.py -t R2 10.0.0.5
+python3 Run.py -ai eth0
+python3 Run.py -v
--- emulation/router4/root/script.sh
+++ emulation/router4/root/script.sh
@@ -1,6 +1,10 @@
 rm -rf MulticastRouting/
 cp -rf /hosthome/Desktop/pim/ MulticastRouting/
 cd MulticastRouting
-pip-3.2 install --index-url=https://pypi.python.org/simple/ -r requirements.txt
 
-python3 Server.py
+
+python3 Run.py -stop
+python3 Run.py -start
+python3 Run.py -t R4 10.0.0.5
+python3 Run.py -ai eth0
+python3 Run.py -v
