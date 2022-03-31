61a28c20b225fc6287ef3430d19218d267b5e17b
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2022-03-31 10:16:32 +0200

    utils.py: Add exit class to handle signals
    exit class can be used to handle signals and do proper cleaning before killing
    a process

--- pimdm/utils.py
+++ pimdm/utils.py
@@ -1,4 +1,4 @@
-import array
+import array, signal, threading, os, sys
 
 
 def checksum(pkt: bytes) -> bytes:
@@ -9,6 +9,45 @@ def checksum(pkt: bytes) -> bytes:
     s += s >> 16
     s = ~s
     return (((s >> 8) & 0xff) | s << 8) & 0xffff
+
+
+class exit(object):
+
+    status = None
+
+    def __init__(self):
+        l = threading.Lock()
+        self.acquire = l.acquire
+        r = l.release
+        def release():
+            try:
+                if self.status is not None:
+                    self.release = r
+                    sys.exit(self.status)
+            finally:
+                r()
+        self.release = release
+
+    def __enter__(self):
+        self.acquire()
+
+    def __exit__(self, t, v, tb):
+        self.release()
+
+    def kill_main(self, status):
+        self.status = status
+        os.kill(os.getpid(), signal.SIGTERM)
+
+    def signal(self, status, *sigs):
+        def handler(*args):
+            if self.status is None:
+                self.status = status
+            if self.acquire(0):
+                self.release()
+        for sig in sigs:
+            signal.signal(sig, handler)
+
+exit = exit()
 
 
 # obtain TYPE_CHECKING (for type hinting)
